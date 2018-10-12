3842d336c1f24e0b09ed6c1c602b7aa84f1054b2
Christopher Torgalson <manager@bedlamhotel.com>
2018-10-12 10:48:14 +0200

    Adds basic Molecule test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -1,14 +1,23 @@
 import os
 
+import pytest
+
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+@pytest.mark.parametrize("path,user,group,mode", [
+    ("/etc/ssl/certs", "root", "root", "0755"),
+    ("/etc/ssl/private", "root", "root", "0755"),
+    ("/etc/ssl/private/www.example.com.key", "root", "root", "0600"),
+    ("/etc/ssl/certs/www.example.com.crt", "root", "root", "0644"),
+])
+def test_ssl_files_and_directories(host, path, user, group, mode):
+    f = host.file(path)
 
     assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+    assert f.user == user
+    assert f.group == group
+    assert oct(f.mode) == mode
