33a6b1fc11cbe88378088487c2ec39e25b02aaaa
Christopher Torgalson <manager@bedlamhotel.com>
2018-10-13 18:11:16 +0200

    Adds new paths to test for self-signed SSL cert files

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -10,9 +10,14 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 @pytest.mark.parametrize("path,user,group,mode", [
     ("/etc/ssl/certs", "root", "root", "0755"),
-    ("/etc/ssl/private", "root", "root", "0755"),
-    ("/etc/ssl/private/www.example.com.key", "root", "root", "0600"),
+    ("/etc/ssl/private", "root", "root", "0700"),
+    ("/etc/ssl/certs/www.example.com.key", "root", "root", "0600"),
     ("/etc/ssl/certs/www.example.com.crt", "root", "root", "0644"),
+    ("/etc/ssl/self/certs", "root", "root", "0755"),
+    ("/etc/ssl/self/private", "root", "root", "0755"),
+    ("/etc/ssl/self/certs/www.example.com-key.pem", "root", "root", "0600"),
+    ("/etc/ssl/self/certs/www.example.com-crt.pem", "root", "root", "0644"),
+    ("/etc/ssl/self/private/www.example.com.csr", "root", "root", "0644"),
 ])
 def test_ssl_files_and_directories(host, path, user, group, mode):
     f = host.file(path)
